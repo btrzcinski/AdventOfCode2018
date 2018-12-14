371b93422885ee51c92ef1acf77805e200fd17b4
Barnett Trzcinski <btrzcinski@gmail.com>
2018-12-14 12:45:27 -0800

    Day 1-2

--- day1_1/src/main.rs
+++ day1_1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -9,18 +10,38 @@ fn open_input_file(file_name: &str) -> File {
     return File::open(&Path::new(file_name)).unwrap();
 }
 
-fn sum_nums_from_file(file: &File) -> i64 {
+fn sum_nums(nums: &Vec<i64>) -> i64 {
+    return nums.into_iter().sum();
+}
+
+fn all_nums_from_file(file: &File) -> Vec<i64> {
     let mut reader = BufReader::new(file);
     let mut buffer = String::new();
-    let mut accumulator = 0;
+    let mut nums = Vec::new();
 
     while reader.read_line(&mut buffer).unwrap_or_default() > 0 {
         let trimmed_line = buffer.trim();
-        accumulator += match i64::from_str_radix(trimmed_line, 10) {
+        let num = match i64::from_str_radix(trimmed_line, 10) {
             Err(why) => panic!("Couldn't parse '{}': {}", trimmed_line, why),
-            Ok(num) => num,
+            Ok(n) => n,
         };
+        nums.push(num);
         buffer.clear();
+    }
+
+    return nums;
+}
+
+fn first_repeated_frequency(nums: &Vec<i64>) -> i64 {
+    let mut accumulator = 0;
+    let mut freqs_seen = HashSet::new();
+
+    for num in nums.into_iter().cycle() {
+        if freqs_seen.contains(&accumulator) {
+            break;
+        }
+        freqs_seen.insert(accumulator);
+        accumulator += num;
     }
 
     return accumulator;
@@ -28,5 +49,7 @@ fn sum_nums_from_file(file: &File) -> i64 {
 
 fn main() {
     let file_name = args().nth(1).unwrap();
-    println!("Sum: {}", sum_nums_from_file(&open_input_file(&file_name)));
+    let all_nums = all_nums_from_file(&open_input_file(&file_name));
+    println!("Sum: {}", sum_nums(&all_nums));
+    println!("First Repeated Freq: {}", first_repeated_frequency(&all_nums));
 }
